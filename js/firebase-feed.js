e03c10bce975c1d8639dbbb90d376c2e001c078a
Rohan Anand <thededlier@gmail.com>
2017-04-07 19:51:45 +0530

    auto-set modal values on request

--- js/firebase-feed.js
+++ js/firebase-feed.js
@@ -9,7 +9,7 @@ function loadEvent(user, key, title, desc, address, region, expire_time, publish
                         '<div class="card-submit">' +
                           '<button class="btn btn-warning">Expires on ' + expire_time + '</button>' +
                           '<button class="btn btn-default">Region : ' + region + '</button>' +
-                          '<button class="btn btn-success pull-right" onclick="clickRequest(' + "'" +  key + "'" + ')">Send Request</button>' +
+                          '<button class="btn btn-success pull-right" onclick="clickRequest(' + "'" + key + "'" + ')">Send Request</button>' +
                         '</div>' +
                       '</div>' +
                     '</div>';
@@ -19,10 +19,33 @@ function loadEvent(user, key, title, desc, address, region, expire_time, publish
 }
 
 function clickRequest(key) {
-  data = firebase.database().ref('feed').child(key);
-
-  $("#sendRequestModal").modal();
+  console.log(key);
+  firebase.database().ref('feed/' + key).on('value', function(data) {
+    $("#sendRequestModal").modal("show");
+    console.log(data);
+    document.getElementById('location').value = data.val().region;
+    document.getElementById('address').value = data.val().address;
+  });
 }
+
+function getContactOptions() {
+  firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+      firebase.database().ref().child('ngos').orderByChild('ngo_email').equalTo(user.email).on('child_added', function(data) {
+        var ngoKey = data.key;
+
+        var html = "";
+        firebase.database.ref('ngos/' + ngoKey + '/contacts/').on('child_added', function(data){
+          ;
+        });
+      });
+    } else {
+      window.location = "./login.html";
+      console.log("user not signed in");
+    }
+  });
+}
+
 firebase.auth().onAuthStateChanged(function(user) {
   if (user) {
     firebase.database().ref('feed').on('child_added', function(data) {
